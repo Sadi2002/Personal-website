5b47300677267968fa30d16e56a59dd5cad2088b
Sadi2002 <lukasz.sadowski2605@gmail.com>
2025-01-26 17:02:23 +0100

    added header on tablet

--- interactivity/script.js
+++ interactivity/script.js
@@ -1,7 +1,9 @@
 // Pobieranie elementów
 const mobileBurger = document.querySelector(".burger-navigation");
+const navMobileList = document.querySelector(".navigation-item");
 
 // Przełączanie klasy active na burgerze
 mobileBurger.addEventListener("click", (e) => {
   e.target.classList.toggle("active");
+  navMobileList.classList.toggle("active");
 });
